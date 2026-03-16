dc2d9a75b7c5d7c25c4afe3f83e7059ccabd3258
Heran <herany119@gmail.com>
2026-03-16 19:42:51 +0200

    Update the bottom navigation bar for role specific options

--- scheduling/urls.py
+++ scheduling/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.client_dashboard, name='home'),
     path('classes/', views.class_list, name='class_list'),
 
     # Booking Workflow
